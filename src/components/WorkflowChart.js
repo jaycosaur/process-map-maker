6ca3cfe517de011ae00ab104f427700cb0b08c07
Jacob Richter <>
2017-12-01 09:48:11 +1100

    Updated WorkflowChart component and re-added on click handler to pass to modal

--- src/components/WorkflowChart.js
+++ src/components/WorkflowChart.js
@@ -11,7 +11,7 @@ var rect_width="100%";
 var element_loc=[],
     grids = [];
 var height_stream_text_div=48; 
-var pad_stream_div=31;
+var pad_stream_div=51;
 
 var decision_rect = "M0.9256479947110743,50.647917796943716 L50.925648235036306,0.6479198530888087 L100.9256484753644,50.647917796943716 L50.925648235036306,100.64791574079848 L0.9256479947110743,50.647917796943716 z";
 var round_rect = "M17.326723591161084,0.9076131555916045 L145.19435658402077,0.9076131555916045 L145.19435658402077,0.9076131555916045 C163.8378728694335,-0.2403311134060704 162.42291142631814,19.962512350924012 162.42291142631814,42.05183005300881 C162.42291142631814,64.14116187477569 161.94537808807442,80.90015841243076 145.19435658402077,80.90015841243076 L17.326723591161084,80.90015841243076 L17.326723591161084,80.90015841243076 C0.10260312417589507,79.17824906877533 0.6154892577233662,62.993217605778014 0.6154892577233662,40.90388578401118 C0.6154892577233662,18.814553962244336 0.10260312417589507,0.9076131555916045 17.326723591161084,0.9076131555916045 z";
@@ -25,10 +25,10 @@ class App extends Component {
     this.createFlowChart = this.createFlowChart.bind(this);
   }
 
-  componentDidMount() {        
+  componentDidMount() {
     var data = this.props.data;
     const context = this.setContext();
-    
+
     try {
         this.createFlowChart(context,data)
     }
@@ -74,6 +74,7 @@ class App extends Component {
         items.push(data.items[e]);
       }
     }
+    svg_height = $('#flowChart_div').height();
     this.span_into_grid(data);
     this.elements_locations(grids);
     this.draw_streams(context, data);
@@ -81,69 +82,145 @@ class App extends Component {
     this.link_objects(data);
   }
 
-  arrange_full_grid(t, e, r) {
-    for (var a = "", s = "", n = "", i = "", o = 0; o < e.length; o++) {
-      for (var d = 0; d < r.length; d++)
-          for (var l = 0; l < r[d].length; l++) {
-            if (r[d][l] === t) {
-              a = d;
-              n = l;
-            }
-            else if (r[d][l] === e[o]) {
-              s = d;
-              i = l;
-            }
+  arrange_full_grid(id,linked_ids,grids) {
+    var id_loc_x="";
+    var linked_loc_x="";
+    var id_loc_y="";
+    var linked_loc_y="";
+    
+    for(var k=0;k<linked_ids.length;k++){
+        
+      for(var i=0;i<grids.length;i++){
+        
+        for(var j=0;j<grids[i].length;j++){
+          
+          if(grids[i][j]==id){
+            id_loc_x=i;
+            id_loc_y=j;
           }
-      if (a !== s && n > i)
-        for (var c = null, p = null, m = Number(n - i), h = 0; m > h; h++) {
-          for (var _ = i; _ < r[s].length; _++) c = r[s][_], null != p ? r[s][_] = p : r[s][_] = null, p = c;
-        }
-    }
-  }
-
-  span_into_grid(t) {
-    var max_streams = 0;
-    var e = {},
-        r = {};
-    $.map(items, function(t, a) {
-        if (t.stream > max_streams) {
-          max_streams = t.stream;
-        }
-        if (!e[t.stream]) {
-          e[t.stream] = [];
-        }
-        e[t.stream].push({
-          id: t.id
-        });
-        if (!r[t.id]) {
-          r[t.id] = [];
-        }
-        $.map(t.connectors, function(e, a) {
-            r[t.id].push(e.linkTo);
-        })
-    });
-
-    grids = new Array(max_streams);
-    for (let a = 0; a < max_streams; a++) {
-      grids[a] = new Array(4);
-      for (let s = 0; 4 > s; s++) 
-        e[a + 1][s] ? grids[a][s] = e[a + 1][s].id : grids[a][s] = null
-    }
-    for (let a = 0; a < max_streams; a++) {
-      for (let s = 0; 4 > s; s++) {
-        if (null != grids[a][s]) {
-          var n = grids[a][s],
-              i = r[n];
-          this.arrange_full_grid(n, i, grids);
+          
+          else if(grids[i][j]==linked_ids[k]){
+            linked_loc_x=i;
+            linked_loc_y=j;
+          }
+        } 
+      }
+      
+      if(id_loc_x!=linked_loc_x){ //if linked is not in same stream or row
+        
+        if(id_loc_y>linked_loc_y){
+          
+          var swap1=null;
+          var swap2=null;
+          
+          var loops=Number(id_loc_y-linked_loc_y);
+          
+          
+          for(var lp=0;lp<loops;lp++){
+                    
+            for(var i2=linked_loc_y;i2<grids[linked_loc_x].length;i2++){
+                        
+              swap1=grids[linked_loc_x][i2];
+              
+              if(swap2!=null){
+                grids[linked_loc_x][i2]=swap2;
+                
+              }
+              
+              else{
+                grids[linked_loc_x][i2]=null;
+              }
+              
+              swap2=swap1;
+              
+            }
+            
+          }
         }
       }
     }
+  }
+
+  span_into_grid(data) {
+    var max_streams = 0;
+    max_streams=0;
+  
+    var items_stream_wise={};
+    
+    var items_connected_to={};
+    
+    $.map(items,function(element,index){
+      
+      if(element.stream>max_streams){
+        max_streams=element.stream;
+      }
+      
+      
+      if(!items_stream_wise[element.stream]){
+        items_stream_wise[element.stream]=[];
+      }
+      
+      items_stream_wise[element.stream].push({"id":element.id});
+      
+      
+      //--------items_connected_to-----
+        
+        if(!items_connected_to[element.id]){
+          items_connected_to[element.id]=[];
+        }
+        
+        $.map(element.connectors,function(d,i){
+          
+          items_connected_to[element.id].push(d.linkTo);
+        });
+        
+      //------------
+      
+    });
+
+    grids=new Array(max_streams);
+  
+    for(var i=0;i<max_streams;i++){
+      
+      grids[i]=new Array(30);
+      
+      for(var j=0;j<30;j++){
+        
+        if(items_stream_wise[i+1][j]){
+          grids[i][j]=items_stream_wise[i+1][j]['id'];
+        }
+        else{
+          grids[i][j]=null;
+        }
+      }
+    }
+
+    for(var i=0;i<max_streams;i++){
+    
+    
+    for(var j=0;j<30;j++){
+      
+      if(grids[i][j]!=null){
+        
+        var check_links=grids[i][j];
+        
+        var process_arr=items_connected_to[check_links];
+        
+        //console.log(process_arr);
+        
+        this.arrange_full_grid(check_links,process_arr,grids); //(element id, element linkTo, grid to be sorted)
+      }
+    }
+    
+  }
   }
 
   // draws streams 
   draw_streams(context, t) {
     for (var e in t.streams) 
       t.streams.hasOwnProperty(e) && streams.push(t.streams[e].title);
+
+    rect_height = svg_height / (streams.length);
 
     var r = context
               .selectAll("rect")
@@ -163,67 +240,198 @@ class App extends Component {
 
     r.exit().remove();
 
-    var a = d3.select("#flowChart_div")
-              .append("div")
-              .attr("class", "streams_text_main_div")
-              .selectAll("div")
-              .data(streams);
+    r.enter()
+      .append("rect")
+      .attr("x", 0)
+      .attr("y", function(t, e) {
+          return e * rect_height
+      })
+      .attr("width", "7%")
+      .attr("height", rect_height)
+      .attr("fill", "#f9f9f9")
+      .style("stroke-width", "1px")
+      .style("stroke", "#ccc");
 
-    a.enter()
-      .append("div")
-      .style("width", rect_height - 2 + "px")
-      .style("padding-top", height_stream_text_div / 2 + "px")
-      .style("padding-bottom", height_stream_text_div / 2 + "px")
-      .style("text-align", "center")
-      .attr("class", "rotate_270")
-      .style("-ms-transform", function(t, e) {
-          return "translate(-" + (pad_stream_div - 2) + "px," + (pad_stream_div + 2 * pad_stream_div * e) + "px) rotate(269.9deg)"
+    r.enter()
+      .data(streams)
+      .append("text")
+      .attr("x", 0)
+      .attr("y", function (t, e) {
+        return e * rect_height
       })
-      .style("-webkit-transform", function(t, e) {
-          return "translate(-" + (pad_stream_div - 2) + "px," + (pad_stream_div + 2 * pad_stream_div * e) + "px) rotate(269.9deg)"
+      .attr("font-size", "16px")
+      .attr("transform", function(t, e) {
+        return 'translate(' + (e * rect_height + 35) + ',' + (e * rect_height + 40) + ') rotate(90)';
       })
-      .style("transform", function(t, e) {
-          return "translate(-" + (pad_stream_div - 2) + "px," + (pad_stream_div + 2 * pad_stream_div * e) + "px) rotate(269.9deg)"
+      .text(function (d) {
+        return d;
       })
-      .text(function(t, e) {
-          return t
-      });
 
-    a.exit().remove();
+
+    // var a = d3.select("#flowChart_div")
+    //           .append("div")
+    //           .attr("class", "streams_text_main_div")
+    //           .selectAll("div")
+    //           .data(streams);
+
+    // a.enter()
+    //   .append("div")
+    //   .style("width", rect_height - 2 + "px")
+    //   .style("padding-top", height_stream_text_div / 2 + "px")
+    //   .style("padding-bottom", height_stream_text_div / 2 + "px")
+    //   .style("text-align", "center")
+    //   .attr("class", "rotate_270")
+    //   .style("-ms-transform", function(t, e) {
+    //       return "translate(-" + (pad_stream_div - 2) + "px," + (pad_stream_div + 2 * pad_stream_div * e) + "px) rotate(269.9deg)"
+    //   })
+    //   .style("-webkit-transform", function(t, e) {
+    //       return "translate(-" + (pad_stream_div - 2) + "px," + (pad_stream_div + 2 * pad_stream_div * e) + "px) rotate(269.9deg)"
+    //   })
+    //   .style("transform", function(t, e) {
+    //       return "translate(-" + (pad_stream_div) + "px," + (pad_stream_div + 2 * pad_stream_div * e) + "px) rotate(269.9deg)"
+    //   })
+    //   .text(function(t, e) {
+    //       return t
+    //   });
+
+    // a.exit().remove();
 
     $(".streams_text_main_div").css("height", rect_height * streams.length + "px");
     $("#flowChart_div svg").attr("height", rect_height * streams.length);
   }
 
-  elements_locations(t) {
-    for (var e = 0; e < t.length; e++)
-        for (var r = 0; r < t[e].length; r++) null != t[e][r] && (element_loc[t[e][r]] = r);
+  elements_locations(grids) {
+    for(var i=0;i<grids.length;i++){
+      
+      for(var j=0;j<grids[i].length;j++){
+        
+        if(grids[i][j]!=null){
+          element_loc[grids[i][j]]=j;
+        }
+      }
+        
+    }
   }
 
   //draws nodes
 
-  draw_objects(context, t) {
-    var e = "",
-        r = [],
-        a = context.selectAll("g").data(items);
-    a = a.enter().append("g").attr("transform", function(t, a) {
-        e = t.stream - 1, r[e] ? r[e] = 300 * element_loc[t.id] : r[e] = 300 * element_loc[t.id];
-        var s = r[e];
-        return "start" === t.type.toLowerCase() || "finish" === t.type.toLowerCase() || "process-simple" === t.type.toLowerCase() ? "translate(" + (100 + s) + "," + (e * rect_height + 25) + ")" : "connector-end" === t.type.toLowerCase() || "connector-start" === t.type.toLowerCase() ? "translate(" + (132 + s) + "," + (e * rect_height + 18) + ")" : "delay" === t.type.toLowerCase() ? "translate(" + (100 + s) + "," + (e * rect_height + 25) + ")" : "decision" === t.type.toLowerCase() ? "translate(" + (131 + s) + "," + (e * rect_height + 15) + ")" : 0
-    }), a.append("path").attr("class", "objects").attr("id", function(t, e) {
-        return "shape_" + t.id
-    }).attr("d", function(t, e) {
-        return "start" === t.type.toLowerCase() || "finish" === t.type.toLowerCase() ? round_rect : "process-simple" === t.type.toLowerCase() ? full_rect : "decision" === t.type.toLowerCase() ? decision_rect : "connector-start" === t.type.toLowerCase() || "connector-end" === t.type.toLowerCase() ? connector_circle : "delay" === t.type.toLowerCase() ? delay_shape : 0
-    }).attr("fill", "#fff").style("stroke-width", "2px").style("stroke", function(t, e) {
-        return "start" === t.type.toLowerCase() || "process-simple" === t.type.toLowerCase() || "connector-start" === t.type.toLowerCase() || "delay" === t.type.toLowerCase() ? "#0088ce" : "finish" === t.type.toLowerCase() || "decision" === t.type.toLowerCase() ? "#f36d00" : "connector-end" === t.type.toLowerCase() ? "#ea0000" : "#000"
-    }).on('click',function(d){
-      this.props.nodeCallBack(d)
-    }.bind(this));
-    a.append("foreignObject").append("xhtml:body").html(function(t) {
-        return "decision" === t.type ? '<div style="width:100px;padding:10px;margin-top:25px;text-align:center;    max-height: 50px;overflow: hidden;">' + t.title + "</div>" : "connector-start" === t.type || "connector-end" === t.type ? '<div style="width:98px;padding:5px;margin-top:15px;text-align:center;    max-height: 50px;overflow: hidden;">' + t.title + "</div>" : '<div style="width:170px;padding:10px;">' + t.title + "</div>"
-    }).on('click',function(d){
-      this.props.nodeCallBack(d)
-    }.bind(this))
+  draw_objects(context, data) {
+    var stream_level="";
+    var item_x_axis_position_streamWise=[];
+    var object_items = d3.select("#flowChart_div")
+              .select("svg").selectAll("g").data(items);
+
+    object_items=object_items
+      .enter()
+      .append("g")
+      .attr("transform",function(d,i){
+        
+        stream_level=d.stream-1;
+        
+        if(item_x_axis_position_streamWise[stream_level]){
+          item_x_axis_position_streamWise[stream_level]=(300*element_loc[d.id]);
+        }
+        else{
+          item_x_axis_position_streamWise[stream_level]=(300*element_loc[d.id]);
+        }
+        
+        var tx=item_x_axis_position_streamWise[stream_level];
+        //console.log(tx);
+        
+        if(d.type.toLowerCase()=="start"||d.type.toLowerCase()=="finish"||d.type.toLowerCase()=="process-simple"){
+          return "translate("+(98+tx)+","+((stream_level*rect_height)+25)+")";
+        }
+        
+        else if(d.type.toLowerCase()=="connector-end"||d.type.toLowerCase()=="connector-start"){
+          return "translate("+(129+tx)+","+((stream_level*rect_height)+18)+")";
+        }
+        
+        else if(d.type.toLowerCase()=="delay"){
+          return "translate("+(100+tx)+","+((stream_level*rect_height)+25)+")";
+        }
+        
+        else if(d.type.toLowerCase()=="decision"){
+          return "translate("+(129+tx)+","+((stream_level*rect_height)+15)+")";
+        }
+        
+        else{
+          return 0; 
+        }
+      }).on('click',function(d){
+        this.props.nodeCallBack(d)
+      }.bind(this));;
+
+    object_items.append("path")
+  
+     .attr("class","objects")
+     
+     .attr("id",function(d,i){ return "shape_"+d.id;})
+       
+    .attr("d", function(d,i){
+
+      if(d.type.toLowerCase()=="start"||d.type.toLowerCase()=="finish"){
+        return round_rect;  
+      }
+      
+      else if(d.type.toLowerCase()=="process-simple"){
+        return full_rect;
+      }
+      
+      else if(d.type.toLowerCase()=="decision"){
+        return decision_rect;
+      }
+      
+      else if(d.type.toLowerCase()=="connector-start"||d.type.toLowerCase()=="connector-end"){
+        
+        return connector_circle;
+      }
+      
+      else if(d.type.toLowerCase()=="delay"){
+        return delay_shape;
+      }
+      
+      else{
+        return 0; 
+      }
+      
+    })
+    .attr("fill", "#fff")
+    .style("stroke-width","2px")
+    .style("stroke",function(d,i){
+
+      if(d.type.toLowerCase()=="start"||d.type.toLowerCase()=="process-simple"||d.type.toLowerCase()=="connector-start"||d.type.toLowerCase()=="delay"){
+        return "#0088ce";   
+      }
+      
+      else if(d.type.toLowerCase()=="finish"||d.type.toLowerCase()=="decision"){
+        return "#f36d00";   
+      }
+      
+      else if(d.type.toLowerCase()=="connector-end"){
+        return "#ea0000";
+      }
+      
+      else{
+        return "#000";  
+      }
+    });
+
+    object_items.append("foreignObject")
+    .append("xhtml:div")
+    .html(function(d){
+      
+      if(d.type=="decision"){
+        return '<div style="width:100px;padding:10px;margin-top:25px;text-align:center;    max-height: 50px;overflow: hidden;">'+d.title+'</div>';
+      }
+      
+      else if(d.type=="connector-start"||d.type=="connector-end"){
+        return '<div style="width:98px;padding:5px;margin-top:15px;text-align:center;    max-height: 50px;overflow: hidden;">'+d.title+'</div>';
+      }
+      
+      else{
+        return '<div style="width:170px;padding:10px;">'+d.title+'</div>';
+      }
+    });
   }
 
   //draws connectors

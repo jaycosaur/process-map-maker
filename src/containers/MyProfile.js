474e1eb7b4c5f6db56150fc857c3bef9519d4931
Jacob Richter <>
2018-02-21 10:29:06 +1100

    Design update for profile page, containerised components

--- src/containers/MyProfile.js
+++ src/containers/MyProfile.js
@@ -4,128 +4,145 @@ import {
   FormControl,
   ControlLabel,
   PageHeader,
-  Grid, Row, Col, Panel
+  Grid, Panel
 } from "react-bootstrap";
 import LoaderButton from "../components/LoaderButton";
 import "./MyProfile.css";
+
+import { Card, Row, Col, Form, Input, Icon, Button } from 'antd'
+
 import { resetPassword } from "../libs/awsLib";
 
-export default class Signup extends Component {
-  constructor(props) {
-    super(props);
+const FormItem = Form.Item;
 
-    this.state = {
-      isLoading: false,
-      oldPassword: "",
-      password: "",
-      confirmPassword: "",
-    };
-  }
+export default (props) =>  
+  <div className="MyProfile">
+    <Row gutter={{ xs: 0, sm: 16 }} type="flex">
+      <Col xs={{span: 24, order: 2}} md={{span: 16, order: 1}}>
+        <ProfileContainer />
+        <SubscriptionContainer />
+        <BillingContainer />
+      </Col>
+      <Col xs={{span: 24, order: 1}} md={{span: 8, order: 2}}>
+        <PasswordContainer />
+      </Col>
+    </Row>
+  </div>
 
-  validateForm() {
+const cardStyle = {marginBottom: 16}
+
+class ProfileContainer extends Component {
+  render() {
     return (
-        this.state.oldPassword.length > 0 &&      
-        this.state.password.length > 0 &&
-        this.state.confirmPassword.length > 0 &&
-        this.state.password === this.state.confirmPassword
-    );
+      <Card style={cardStyle} className="col-card" loading title="Profile Information"></Card>
+    )
   }
+}
 
-  passwordCriteria(){
-    var strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{12,})");
-    var mediumRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})");
-    if((this.state.password.length > 0) && strongRegex.test(this.state.password)) {
-      return 'success';
-    } else if((this.state.password.length > 0) && mediumRegex.test(this.state.password)) {
-      return 'warning';
-    } else if ((this.state.password.length > 0)){
-      return 'error';
-    }
+class SubscriptionContainer extends Component {
+  render() {
+    return (
+      <Card style={cardStyle} className="col-card" loading title="Subscription Information"></Card>
+    )
   }
+}
 
-  passwordsMatch() {
-    if ((this.state.confirmPassword.length > 0) && (this.passwordCriteria() !== "error") && (this.state.password === this.state.confirmPassword)) return 'success';
-    else if (this.state.confirmPassword.length > 0) return 'error';
+class BillingContainer extends Component {
+  render() {
+    return (
+      <Card style={cardStyle} className="col-card" loading title="Billing Information"></Card>
+    )
   }
+}
 
-  handleChange = event => {
-    this.setState({
-      [event.target.id]: event.target.value
-    });
-  }
+class PasswordContainer extends Component {
 
-  handleSubmit = async event => {
-    event.preventDefault();
+  handleSubmit = async (data) => {
     this.setState({ isLoading: true });
-    await resetPassword(this.state.oldPassword, this.state.password);
+    await resetPassword(data.oldPassword, data.password);
     this.setState({ isLoading: false });
-    alert("Successfully changed password!");    
-    this.props.history.push("/");    
-  }
-
-  renderForm() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <FormGroup controlId="oldPassword" bsSize="lg">
-          <ControlLabel>Old Password</ControlLabel>
-          <FormControl
-            value={this.state.oldPassword}
-            onChange={this.handleChange}
-            type="password"
-          />
-        </FormGroup>
-        <FormGroup controlId="password" bsSize="lg" validationState={this.passwordCriteria()}>
-          <ControlLabel>New Password</ControlLabel>
-          <FormControl
-            value={this.state.password}
-            onChange={this.handleChange}
-            type="password"
-          />
-        </FormGroup>
-        <FormGroup controlId="confirmPassword" bsSize="lg" validationState={this.passwordsMatch()}>
-          <ControlLabel>Confirm New Password</ControlLabel>
-          <FormControl
-            value={this.state.confirmPassword}
-            onChange={this.handleChange}
-            type="password"
-          />
-        </FormGroup>
-        <LoaderButton
-          block
-          bsSize="large"
-          disabled={!this.validateForm()}
-          type="submit"
-          isLoading={this.state.isLoading}
-          text="Change Password"
-          loadingText="Changing…"
-        />
-      </form>
-    );
-  }
-
-  renderUserInfo() {
-    return (
-      <p>Nothing to display.</p>
-    );
+    //alert("Successfully changed password!");    
   }
 
   render() {
     return (
-      <div className="MyProfile">
-        <PageHeader>My Profile</PageHeader>
-        <Grid>
-          <Row>
-            <Col xs={12} md={6}>
-              <Panel header="Profile Information" bsStyle="primary">{this.renderUserInfo()}</Panel>
-              <Panel header="Subscription Information">{this.renderUserInfo()}</Panel>
-              <Panel header="Billing Information">{this.renderUserInfo()}</Panel>
-            </Col>
-            <Col xs={12} md={6}>
-              <Panel header="Change Password">{this.renderForm()}</Panel>
-            </Col>
-          </Row>
-        </Grid>
-      </div>
+      <Card title="Change Password">
+        <WrappedRegistrationForm handleSubmit={this.handleSubmit}/>
+      </Card>
+    )
+  }
+}
+
+class RegistrationForm extends React.Component {
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        this.props.handleSubmit(values)
+      }
+    });
+  }
+
+  checkPassword = (rule, value, callback) => {
+    const form = this.props.form;
+    if (value && value !== form.getFieldValue('password')) {
+      callback('Passwords that you enter must be the same!');
+    } else {
+      callback();
+    }
+  }
+
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    const regexp = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})")
+
+    return (
+      <Form onSubmit={this.handleSubmit}>
+        <FormItem>
+          {getFieldDecorator('oldpassword', {
+            rules: [{
+              required: true, message: 'Please input your existing password!',
+            }],
+          })(
+            <Input size="large" prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Old Password" />
+          )}
+        </FormItem>
+        <FormItem>
+          {getFieldDecorator('password', {
+            rules: [{
+              required: true, message: 'Please input your password!',
+            },{
+              pattern: regexp, message: 'Your password does not satisfy the requirements. Must contain 1 lowercase letter, 1 uppercase letter, 1 numeral, 1 special character, and must be at least 8 characters long.',
+            }],
+          })(
+            <Input size="large" prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="New Password" />
+          )}
+        </FormItem>
+        <FormItem
+        >
+          {getFieldDecorator('confirmPassword', {
+            rules: [{
+              required: true, message: 'Please confirm your password!',
+            }, {
+              validator: this.checkPassword,
+            }],
+          })(
+            <Input size="large" onBlur={this.handleConfirmBlur} prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Confirm new password" />
+          )}
+        </FormItem>
+        
+        <FormItem>
+          <div style={{display: "flex"}}>
+            <Button htmlType="submit">Change Password</Button>
+          </div>
+        </FormItem>
+      </Form>
     );
   }
 }
+
+const WrappedRegistrationForm = Form.create()(RegistrationForm);
+
+
+
